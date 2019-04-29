9af8da078e432cbf054a44901298158521e72368
Nicholas Machin <nicholasjohnmachin@gmail.com>
2019-04-29 20:04:41 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_push(arr, str)
+  arr.push(str)
+end
+
+def using_unshift(arr, str)
+  arr.unshift(str)
+end
+
+def using_pop(arr)
+  arr.pop()
+end
+
+def pop_with_args(arr)
+  arg = []
+  arg << arr.pop()
+  arg << arr.pop()
+  arg
+end
+
+def using_shift(arr)
+  arr.shift()
+end
+
